de038459ae0d4096b6b3248976099c73a534cb52
Ging Louie <ging.m.louie@gmail.com>
2023-06-03 05:42:56 -0700

    Commit add'l base and home code

--- core/settings.py
+++ core/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.views.categories',
             ],
         },
     },
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media/')
--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from .models import Product, Category
 
+def categories(request):
+    categories = Category.objects.all()
+    return {'categories': categories}
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
