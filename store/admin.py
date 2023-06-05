fbecc212997ec421b1182ce0ff78d4889304978f
Ging Louie <ging.m.louie@gmail.com>
2023-06-05 07:44:04 -0700

    Commit category list, all poducts and product details functionality

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+
 from .models import Category, Product
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
-    
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'in_stock', 'created', 'updated']
@@ -13,5 +16,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_editable = ['price', 'in_stock']
     prepopulated_fields = {'slug': ('name',)}
     raw_id_fields = ['category']
-    #date_hierarchy = 'created'
-    #ordering = ['name', 'price']
+    # date_hierarchy = 'created'
+    # ordering = ['name', 'price']
--- store/models.py
+++ store/models.py
@@ -1,15 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(available=True)
+
 
 class Category(models.Model):
-    name  =  models.CharField(max_length=255, db_index=True)
+    name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
-     
+
     class Meta:
         verbose_name_plural = 'categories'
-         
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
     def __str__(self):
         return self.name
-    
+
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255, db_index=True)
@@ -21,15 +33,18 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    NOTE= models.TextField(blank=True)
+    NOTE = models.TextField(blank=True)
     in_stock = models.BooleanField(default=True)
-     
+    objects = models.Manager()
+    products = ProductManager()
+
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('name',)
         index_together = (('id', 'slug'),)
-         
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+
     def __str__(self):
         return self.name
-    
-     
